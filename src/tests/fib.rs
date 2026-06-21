dc2b4b3543dfda73a43147d7bce81f373cff55a0
Alex Zenla <alex@edera.dev>
2026-06-20 22:39:14 -0700

    fib running test

--- src/tests/fib.rs
+++ src/tests/fib.rs
@@ -1,3 +1,6 @@
+use crate::machine::ops::OpHandlerSet;
+use crate::machine::value::MachineValue;
+use crate::machine::{Machine, MachineProgram, ops};
 use crate::program::RawProgram;
 use std::str::FromStr;
 
@@ -14,4 +17,27 @@ fn survives_encode_decode() {
     let encoded = constants::FIB_PROGRAM.encode();
     let decoded = RawProgram::decode(&encoded).unwrap();
     assert_eq!(decoded, constants::FIB_PROGRAM);
+}
+
+fn run_fib_program(ops: OpHandlerSet, program: MachineProgram) -> MachineValue {
+    let mut machine = Machine::new(ops);
+    machine.state().push(MachineValue::Uint64(30));
+    machine.run(&program).unwrap();
+    machine.state().pop().unwrap()
+}
+
+#[test]
+fn runs_uncompiled() {
+    let program = MachineProgram::Uncompiled(&constants::FIB_PROGRAM);
+    let value = run_fib_program(ops::all(), program);
+    assert_eq!(value, MachineValue::Uint64(832040));
+}
+
+#[test]
+fn runs_inlined() {
+    let ops = ops::all();
+    let inlined = ops.inline(&constants::FIB_PROGRAM).unwrap();
+    let program = MachineProgram::Inlined(inlined);
+    let value = run_fib_program(ops::all(), program);
+    assert_eq!(value, MachineValue::Uint64(832040));
 }
