5cb50953d9d61a1601c77ac27879647586203219
Alex Zenla <alex@edera.dev>
2026-07-19 03:40:03 -0700

    run programs with main

--- src/machine/value.rs
+++ src/machine/value.rs
@@ -1,3 +1,5 @@
+use crate::op::OpArg;
+
 mod numerics;
 
 #[derive(Clone, Copy, Debug, Default)]
@@ -13,4 +15,21 @@ pub enum MachineValue {
     Int32(i32),
     Int64(i64),
     ReturnAddress(usize),
+}
+
+impl From<OpArg> for MachineValue {
+    fn from(value: OpArg) -> Self {
+        match value {
+            OpArg::None => MachineValue::None,
+            OpArg::Uint8(value) => MachineValue::Uint8(value),
+            OpArg::Uint16(value) => MachineValue::Uint16(value),
+            OpArg::Uint32(value) => MachineValue::Uint32(value),
+            OpArg::Uint64(value) => MachineValue::Uint64(value),
+            OpArg::Int8(value) => MachineValue::Int8(value),
+            OpArg::Int16(value) => MachineValue::Int16(value),
+            OpArg::Int32(value) => MachineValue::Int32(value),
+            OpArg::Int64(value) => MachineValue::Int64(value),
+            _ => MachineValue::None,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
+use std::path::Path;
+use std::str::FromStr;
 use tinyvm::machine::error::Result;
+use tinyvm::machine::value::MachineValue;
+use tinyvm::machine::{Machine, MachineProgram, ops};
+use tinyvm::op::OpArg;
+use tinyvm::program::RawProgram;
 
 fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().map(|arg| arg.to_string()).collect();
+    if args.len() < 2 {
+        eprintln!("usage: tinyvm <program>");
+        std::process::exit(1);
+    }
+    let path = Path::new(&args[1]);
+
+    if !path.exists() {
+        eprintln!("file '{}' does not exist", path.display());
+        std::process::exit(1);
+    }
+
+    let content = std::fs::read_to_string(path).unwrap();
+    let program = RawProgram::from_str(&content).unwrap();
+    let ops = ops::all();
+    let inlined = ops.inline(&program)?;
+    let program = MachineProgram::Inlined(inlined);
+    let mut machine = Machine::new(ops);
+
+    for arg in args.iter().skip(2) {
+        let argument = OpArg::from_str(arg).unwrap();
+        let value: MachineValue = argument.into();
+        machine.state().push(value);
+    }
+
+    machine.run(&program)?;
+    let result = machine.state().pop()?;
+    println!("{:?}", result);
     Ok(())
 }
